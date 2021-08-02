349fd7d1c858edf0ebd44359b1f1bae1c35268f2
DESKTOP-1MGS9EP\Bijay <bijaythakur330@gmail.com>
2021-08-02 23:26:10 +0545

    initial commit

---
+++ app/src/main/java/com/biijay/MainActivity.java
@@ -0,0 +1,20 @@
+package com.biijay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.buttondis);
+
+        button.setEnabled(false);
+    }
+}
